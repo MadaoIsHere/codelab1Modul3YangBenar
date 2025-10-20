177c1b5112ab2925814f17012e086db00fb604c8
MadaoIsHere <tungau699@gmail.com>
2025-10-20 08:18:36 +0700

    Codelab1Modul2

---
+++ src/codelab1.java
@@ -0,0 +1,20 @@
+public class codelab1 {
+
+        public static void main(String[] args) {
+            String[][] jadwal = {
+                    {"Senin", "Matematika Diskrit", "08:00 - 09:40"},
+                    {"Selasa", "Logika dan Komputasi", "10:00 - 11:40"},
+                    {"Rabu", "Aljabar Linear dan Matriks", "09:00 - 10:40"},
+                    {"Kamis", "Pemrograman Lanjut", "13:00 - 14:40"},
+                    {"Jumat", "Sistem Operasi", "08:00 - 09:40"}
+            };
+
+            System.out.println("Jadwal Kuliah Minggu Ini:");
+            System.out.println("----------------------------");
+            for (String[] mataKuliah : jadwal) {
+                System.out.printf("%-10s | %-25s | %s%n", mataKuliah[0], mataKuliah[1], mataKuliah[2]);
+            }
+        }
+
+
+}
